fa896cb15a99284e1a5e38e09bf7a004d00a9039
chimey <selamet.antsoftmedia@gmail.com>
2023-10-11 21:35:59 +0700

    fix

--- app/Services/HalamanDepan/RepositoryInovationService.ts
+++ app/Services/HalamanDepan/RepositoryInovationService.ts
@@ -5,22 +5,23 @@ class RevositoryInovationService {
   protected Model = Inovation
 
   public async lists(){
-    const kompetisi = await this.Model.query().preload('kompetisi').whereIn("status",[5,6]).orderBy("id",'desc')
+    const kompetisi = await this.Model.query().preload('kompetisi').preload('regency').whereIn("status",[5,6]).orderBy("id",'desc')
 
     const datas:{}[]=[]
 
-    let i = 0
+    let i = 1
     kompetisi.forEach(element => {
       const row = {}
       row['id']= element.uuid
       row['num']= i++
       row['name']= element.name
       row['inovator'] = element.inovatorNama
+      row['kabupaten']= element.regency ? element.regency.name :""
       datas.push(row)
     });
 
     //load data sinovic
-    const sinovic = await Sinovic.query().preload('kompetisi').whereIn("status",[5,6]).orderBy("id",'desc')
+    const sinovic = await Sinovic.query().preload('kompetisi').preload('regency').whereIn("status",[5,6]).orderBy("id",'desc')
 
     sinovic.forEach(element => {
       const row = {}
@@ -28,6 +29,7 @@ class RevositoryInovationService {
       row['num']=i++
       row['name']= element.name
       row['inovator'] = element.inovatorNama
+      row['kabupaten']= element.regency ? element.regency.name :""
       datas.push(row)
     });
 
